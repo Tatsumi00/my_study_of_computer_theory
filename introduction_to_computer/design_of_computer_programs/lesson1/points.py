0290b9954cefc333b2df21e63950a4e54667f29b
Tatsumi <waynezhang0@outlook.com>
2019-06-10 16:56:33 +0800

    points

---
+++ introduction_to_computer/design_of_computer_programs/lesson1/points.py
@@ -0,0 +1,35 @@
+#beginning
+# 自己对于问题的理解<=>标准答案
+# 存在不同
+# 分析，理解区别
+# 学习不知道的重点，新知识
+#
+# 观察学习
+# 应用
+# 交流学会
+#
+# 设计过程
+# ??->problem->spec->codes
+# 模糊概念（收集信息）
+# 问题规范描述（整合信息）
+# 代码化（转化成变量，方法）
+# 代码运行
+
+#ending
+# efficiency
+# correctness
+# features
+# elegance:clarity.simplicity.generality
+# !!!tradeoff
+# which direction to move
+#
+# #elegance
+# refactor
+# don't repeat yourself
+#
+# summary
+# understand
+# define pieces
+# reuse module
+# test
+# explore
